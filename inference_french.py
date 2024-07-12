f4b282a2eb45efa10d2690dd1e20da918a5c2028
Berat Denizdurduran <bdenizdurduran@gmail.com>
2024-07-12 11:44:40 +0200

    FEAT > with training dataset

--- inference_french.py
+++ inference_french.py
@@ -9,7 +9,7 @@ st.title("Pratiquer le francais")
 
 @st.cache_data
 def load_data():
-    df = pd.read_json('francais_cinquante.json', orient ='split')
+    df = pd.read_json('francais_training_dataset.json', orient ='split')
     return df
 
 def select_item(df):
